e23b6e7c4144a2fe4af7dea3b8ba8045b25d3318
Ashley Laisure <ashley.laisure@gmail.com>
2025-08-08 16:59:22 -0400

    Added Tag, Note, Collections, Interactions Models

---
+++ database/collection.model.ts
@@ -0,0 +1,16 @@
+import { model, models, Schema, Types, Document } from "mongoose";
+
+export interface ICollection {
+    author: Types.ObjectId;
+    questions: Types.ObjectId;
+}
+
+export interface ICollectionDoc extends ICollection, Document {}
+const CollectionSchema = new Schema<ICollection>({
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    questions: { type: Schema.Types.ObjectId, ref: "Question" },
+}, { timestamps: true });
+
+const Collection = models?.Collection || model<ICollection>("Collection", CollectionSchema);
+
+export default Collection;
--- database/user.model.ts
+++ database/user.model.ts
@@ -1,28 +1,31 @@
-import { model, models, Schema } from "mongoose";
+import { model, models, Schema, Document } from "mongoose";
 
 export interface IUser {
     name: string;
     username: string;
     email: string;
     bio?: string;
-    image: string;
+    image?: string;
     location?: string;
     portfolio?: string;
     reputation?: number;
 }
 
-const userSchema = new Schema({
+export interface IUserDoc extends IUser, Document {}
+
+const UserSchema = new Schema({
     name: {type: String, required: true},
     username: {type: String, required: true, unique: true},
     email: {type: String, required: true, unique: true},
     bio: {type: String},
     image: {type: String, required: true},
     location: {type: String},
+    // change to loc?
     portfolio: {type: String},
     reputation: {type: Number, default: 0},
     },
     { timestamps: true }
 );
 
-const User = models.User || model<IUser>("User", userSchema);
+const User = models.User || model<IUser>("User", UserSchema);
 export default User;
