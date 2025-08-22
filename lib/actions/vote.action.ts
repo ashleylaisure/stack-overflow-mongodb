de07aa8aaeb28ae206b7a9c2ff21f4fd6950bc78
Ashley Laisure <ashley.laisure@gmail.com>
2025-08-22 15:12:55 -0400

    Implemented Has Voted Action

--- lib/actions/vote.action.ts
+++ lib/actions/vote.action.ts
@@ -2,7 +2,7 @@
 import mongoose, { ClientSession } from "mongoose";
 import action from "../handlers/action";
 import handleError from "../handlers/error";
-import { CreateVoteSchema, UpdateVoteCountSchema } from "../validations";
+import { CreateVoteSchema, hasVotedSchema, UpdateVoteCountSchema } from "../validations";
 import { Answer, Question, Vote } from "@/database";
 
 export async function updateVoteCount(
@@ -105,10 +105,55 @@ async function createVote(params: CreateVoteParams): Promise<ActionResponse>{
         session.endSession();
 
         return { success: true };
-        
+
     } catch (error) {
         await session.abortTransaction();
         session.endSession()
         return handleError(error) as ErrorResponse
+    }
+}
+
+export async function hasVoted(params: hasVotedParams): Promise<ActionResponse<HasVotedResponse>>{
+    const validationResult = await action({
+        params,
+        schema: hasVotedSchema,
+        authorize: true,
+    });
+
+    if (validationResult instanceof Error) {
+        return handleError(validationResult) as ErrorResponse;
+    }
+
+    const { targetId, targetType } = validationResult.params!;
+    const userId = validationResult.session?.user?.id;
+
+    if (!userId) {
+        return handleError(new Error("Unauthorized")) as ErrorResponse;
+    }
+
+    try {
+        const vote = await Vote.findOne({
+            author: userId,
+            actionId: targetId,
+            actionType: targetType
+        });
+
+        if(!vote) {
+            return {
+                success: false,
+                data: {hasUpvoted: false, hasDownvoted: false}
+            };
+        }
+
+        return {
+            success: true,
+            data: {
+                hasUpvoted: vote?.voteType === "upvote",
+                hasDownvoted: vote?.voteType === "downvote"
+            }
+        };
+        
+    } catch (error) {
+        return handleError(error) as ErrorResponse;
     }
 }
