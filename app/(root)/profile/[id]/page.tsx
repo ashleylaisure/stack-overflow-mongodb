8c96b829638755b9e2680aaa44cfa4f219afbb5a
Ashley Laisure <ashley.laisure@gmail.com>
2025-08-27 14:56:46 -0400

    Implement User Questions Tab

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { auth } from "@/auth";
-import { getUser } from "@/lib/actions/user.action";
+import { getUser, getUserQuestions } from "@/lib/actions/user.action";
 import { notFound } from "next/navigation";
 import dayjs from "dayjs";
 import { Button } from "@/components/ui/button";
@@ -9,8 +9,16 @@ import ROUTES from "@/constants/routes";
 import ProfileLink from "@/components/user/ProfileLink";
 import Stats from "@/components/user/Stats";
 
-const ProfilePage = async ({ params }: RouteParams) => {
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import DataRenderer from "@/components/DataRenderer";
+import { EMPTY_QUESTION } from "@/constants/states";
+import QuestionCard from "@/components/cards/QuestionCard";
+import Pagination from "@/components/Pagination";
+
+
+const ProfilePage = async ({ params, searchParams }: RouteParams) => {
     const { id } = await params;
+    const {page, pageSize} = await searchParams
 
     if (!id) notFound();
 
@@ -25,6 +33,19 @@ const ProfilePage = async ({ params }: RouteParams) => {
         );
 
     const { user, totalQuestions, totalAnswers } = data!;
+
+    const {
+        success: userQuestionsSuccess,
+        data: userQuestions,
+        error: userQuestionsError,
+    } = await getUserQuestions({ 
+        userId: id, 
+        page: Number(page) || 1, 
+        pageSize: Number(pageSize) || 10
+    })
+
+    const {questions, isNext: hasMoreQuestions} = userQuestions!
+
     const { _id, name, image, portfolio, location, createdAt, username, bio } = user;
 
     return (
@@ -92,6 +113,41 @@ const ProfilePage = async ({ params }: RouteParams) => {
                     BRONZE: 0,
                 }}
             />
+
+            <section className="mt-10 flex gap-10">
+                <Tabs defaultValue="top-posts" className="flex-[2]">
+                    <TabsList className="background-light800_dark400 min-h-[42px] p-1">
+                        <TabsTrigger value="top-posts" className="tab">Top Posts</TabsTrigger>
+                        <TabsTrigger value="answers" className="tab">Answers</TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="top-posts" className="mt-5 felx w-full flex-col gap-6">
+
+                        <DataRenderer
+                            data={questions}
+                            empty={EMPTY_QUESTION}
+                            success={userQuestionsSuccess}
+                            error={userQuestionsError}
+                            render={(hotQuestions) => (
+                                <div className="flex w-full flex-col gap-6">
+                                {questions.map((question) => (
+                                    <QuestionCard key={question._id} question={question} />
+                                ))}
+                                </div>
+                            )}
+                        />
+
+                        <Pagination page={page} isNext={hasMoreQuestions} />
+                    </TabsContent>
+                    <TabsContent value="answers" className="mt-5 felx w-full flex-col gap-6">A List of Answers</TabsContent>
+                </Tabs>
+
+                <div className="flex w-full min-w-[250px] flex-1 flex-col max-lg:hidden">
+                    <h3 className="h3-bold text-dark200_light900">Top Tags</h3>
+                    <div className="mt-7 flex felx-col gap-4">
+                        <p>List of Tags</p>
+                    </div>
+                </div>
+            </section>
         </>
     );
 };
